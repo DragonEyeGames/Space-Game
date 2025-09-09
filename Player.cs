beb40d9e87ac6af9876585aba462ab537f541aec
Splooble <155033535+MrWaffle09@users.noreply.github.com>
2025-09-09 11:58:37 -0400

    build error fix

--- Player.cs
+++ Player.cs
@@ -1,14 +1,14 @@
 using Godot;
 using System;
-#handles movement, mostly godot docs
+//handles movement, mostly godot docs
 public partial class Movement : CharacterBody2D
 {
 	[Export]
-	public int speed {get; set} = 400;
+	public int speed {get; set;} = 400;
 	
 	public void GetInput()
 	{
-		Vector2 inputDirection = Input.GetVector("Left", "Right", "Forward", "Backward")
+		Vector2 inputDirection = Input.GetVector("Left", "Right", "Forward", "Backward");
 		Velocity = inputDirection * speed; 
 	}
 	
