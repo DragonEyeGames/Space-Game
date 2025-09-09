3d42918aa489529afd801cbf0e0ba474c7f49bde
Taxalottle Syndicate <133176852+DragonEyeGames@users.noreply.github.com>
2025-09-09 12:24:34 -0400

    Cool

--- Main.cs
+++ Main.cs
@@ -8,7 +8,12 @@ public partial class Main : Node2D
 	{
 		ColorRect shaderRect = GetNode<ColorRect>("ParallaxBackground/ParallaxLayer/ColorRect");
 		(shaderRect.Material as ShaderMaterial).SetShaderParameter("randomTranslation", new Vector2((float)GD.RandRange(0.0, 10.0), (float)GD.RandRange(0.0, 10.0)));
-		ColorRect shaderRectTwo = GetNode<ColorRect>("ParallaxBackground/ParallaxLayer/ColorRect2");
-		(shaderRectTwo.Material as ShaderMaterial).SetShaderParameter("randomTranslation", new Vector2((float)GD.RandRange(0.0, 10.0), (float)GD.RandRange(0.0, 10.0)));
+		ColorRect shaderRectTwo = GetNode<ColorRect>("ParallaxBackground/ParallaxLayer2/ColorRect");
+		(shaderRectTwo.Material as ShaderMaterial).SetShaderParameter("randomTranslation", new Vector2((float)GD.RandRange(0.0, -10.0), (float)GD.RandRange(0.0, -10.0)));
+	}
+	
+	public override void _Process(double delta)
+	{
+		
 	}
 }
