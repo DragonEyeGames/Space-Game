c59983f7d587c5347a193e99728d89bb558425a0
Taxalottle Syndicate <bthole41@gmail.com>
2025-09-09 20:34:08 -0400

    Shihp

--- StarField.cs
+++ StarField.cs
@@ -5,15 +5,16 @@ using System.Collections.Generic;
 public partial class StarField : Node2D
 {
 	[Export] public int StarCount = 300;
+	[Export] public Texture2D StarTexture; // Assign a circular texture in the Inspector
 	[Export] public Color[] StarColors = new Color[]
 	{
 		new Color(1, 1, 1),        // White
 		new Color(1, 0.95f, 0.9f), // Warm white
 		new Color(0.8f, 0.9f, 1f)  // Cool white
 	};
-	
-	public List<ColorRect> SmallStars = new List<ColorRect>();
-	public List<ColorRect> BigStars = new List<ColorRect>();
+
+	private List<Sprite2D> SmallStars = new List<Sprite2D>();
+	private List<Sprite2D> BigStars = new List<Sprite2D>();
 
 	public override void _Ready()
 	{
@@ -22,58 +23,56 @@ public partial class StarField : Node2D
 
 		for (int i = 0; i < StarCount; i++)
 		{
-			var star = new ColorRect();
-			star.Color = StarColors[GD.Randi() % StarColors.Length];
+			var star = new Sprite2D
+			{
+				Texture = StarTexture,
+				Modulate = StarColors[GD.Randi() % StarColors.Length],
+				ZIndex = -10
+			};
 
-			float size = (float)GD.RandRange(1f, 3f); // Star size 1–3 px
-			if(size <=2) {
+			// Random scale instead of size
+			float scale = (float)GD.RandRange(0.01f, 0.03f); // Scale range
+			star.Scale = new Vector2(scale, scale);
+
+			// Categorize stars
+			if (scale <= 0.4f)
 				SmallStars.Add(star);
-			}
-			else if(size <=3) {
+			else
 				BigStars.Add(star);
-			}
-			star.Size = new Vector2(size, size);
 
 			star.Position = new Vector2(
 				(float)GD.RandRange(0, screenSize.X),
 				(float)GD.RandRange(0, screenSize.Y)
 			);
-			
-			
-			star.ZIndex=-10;
-			
 
+			// Random alpha
 			Color modulate = star.Modulate;
-			modulate.A = (float)GD.RandRange(0.5, 1.0); // Transparency 0.5–1.0
+			modulate.A = (float)GD.RandRange(0.5f, 1.0f);
 			star.Modulate = modulate;
 
 			AddChild(star);
 		}
 	}
-	
+
 	public override void _Process(double delta)
 	{
 		Vector2 screenSize = GetViewport().GetVisibleRect().Size;
 
-		foreach (ColorRect star in SmallStars)
+		foreach (var star in SmallStars)
 		{
-			star.Position += new Vector2(0, 2 * (float)delta);
-
-			// Reset if off-screen
+			star.Position += new Vector2(0, 20f * (float)delta);
 			if (star.Position.Y > screenSize.Y)
 			{
 				star.Position = new Vector2(
 					(float)GD.RandRange(0, screenSize.X),
-					1
+					0
 				);
 			}
 		}
 
-		foreach (ColorRect star in BigStars)
+		foreach (var star in BigStars)
 		{
-			star.Position += new Vector2(0, 5 * (float)delta);
-
-			// Reset if off-screen
+			star.Position += new Vector2(0, 50f * (float)delta);
 			if (star.Position.Y > screenSize.Y)
 			{
 				star.Position = new Vector2(
